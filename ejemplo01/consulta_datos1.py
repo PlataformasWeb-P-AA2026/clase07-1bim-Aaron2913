abbdcb88dd7414006ce08c1b68a6718280b13b2e
Aaron <roblesaaron163@gmail.com>
2026-05-21 11:19:11 -0500

    Cambios

--- ejemplo01/consulta_datos1.py
+++ ejemplo01/consulta_datos1.py
@@ -38,8 +38,10 @@ jugadores = session.query(Jugador).all()
 
 print("Jugadores")
 for s in jugadores:
-    print("Mi jugador es (%s), su club fue fundado en (%d)" % (s.nombre, 
-                                                               s.club.fundacion))
+    print("Mi jugador es (%s), su club fue fundado en (%d) y pertenece a (%s)" % (s.nombre, 
+                                                               s.club.fundacion, s.club.nombre))
+    
+    print(f"Mi jugador es ({s.nombre}), su club fue fundado en ({s.club.fundacion}) y pertenece a ({s.club.nombre})")
     print("---------")
 
 
